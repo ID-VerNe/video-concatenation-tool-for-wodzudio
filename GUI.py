af6d4dccfd80b03218d57f4c8d4a9edbd93ff839
VerNe <49872960+ID-VerNe@users.noreply.github.com>
2024-05-07 20:36:38 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,12 +1,41 @@
+import configparser
+import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-import os
+
+from moviepy.editor import *
+
+
+def load_or_create_config():
+    config = configparser.ConfigParser()
+    config_file_path = 'config.ini'
+    if not os.path.exists(config_file_path):
+        config['DEFAULT'] = {'PieceDuration': '300',
+                             'FrontClip': 'front.mp4',
+                             'EndClip': 'end.mp4',
+                             'OutputFolder': 'output',
+                             'TargetFPS': '30',
+                             'ConcatenateFlag': 'False',
+                             'Height': '872',
+                             'Width': '1920',
+                             'FFmpegCodec': 'nvenc',
+                             'Bitrate': '8000k',
+                             'ConcatenateParam': '2',
+                             'FadeDuration': '2'}
+
+        with open(config_file_path, 'w') as configfile:
+            config.write(configfile)
+    else:
+        config.read(config_file_path)
+
+    return config['DEFAULT']
 
 
 def split_video(video_path, piece_duration=300, front_clip_='front.mp4', end_clip_='end.mp4', output_folder="output",
-                target_fps=25, concatenate_flag=True, height=872, width=1920, ffmpeg_codec="nvenc"):
+                target_fps=25, bitrate='8000k', concatenate_flag=True, height=872, width=1920, ffmpeg_codec="nvenc",
+                concatenate_param=2, fade_duration=2):
     main_video = VideoFileClip(video_path).set_fps(target_fps)
+    main_video = main_video.resize(height=height, width=width)
 
     duration = main_video.duration
     num_pieces = int(duration / piece_duration) + (duration % piece_duration > 0)
@@ -14,30 +43,54 @@ def split_video(video_path, piece_duration=300, front_clip_='front.mp4', end_cli
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    front_clip = VideoFileClip(front_clip_).set_fps(target_fps)
-    end_clip = VideoFileClip(end_clip_).set_fps(target_fps)
+    if not concatenate_flag:
+        if concatenate_param == 2:
+            front_clip = VideoFileClip(front_clip_).set_fps(target_fps)
+            front_clip = front_clip.resize(height=height, width=width)
+            end_clip = VideoFileClip(end_clip_).set_fps(target_fps)
+            end_clip = end_clip.resize(height=height, width=width)
+        else:
+            end_clip = VideoFileClip(end_clip_).set_fps(target_fps)
+            end_clip = end_clip.resize(height=height, width=width)
 
     for i in range(num_pieces):
         start_time = i * piece_duration
         end_time = min((i + 1) * piece_duration, duration)
         piece = main_video.subclip(start_time, end_time)
-        # 设置一个参数用于接收是多少视频合并，true为1，false为3
+        piece = piece.fadein(fade_duration).fadeout(fade_duration)
         if concatenate_flag:
             final_clip = concatenate_videoclips([piece])
         else:
-            final_clip = concatenate_videoclips([front_clip, piece, end_clip])
+            if concatenate_param == 2:
+                final_clip = concatenate_videoclips([front_clip, piece, end_clip])
+            else:
+                final_clip = concatenate_videoclips([piece, end_clip])
         piece_filename = f"{output_folder}/output_{i + 1}.mp4"
-        final_clip.write_videofile(piece_filename, codec=ffmpeg_codec, ffmpeg_params=['-vf', f'scale={width}:{height}'])
+        final_clip.write_videofile(piece_filename, codec=ffmpeg_codec, bitrate=bitrate,
+                                   ffmpeg_params=['-vf', f'scale={width}:{height}'])
 
     main_video.close()
-    front_clip.close()
-    end_clip.close()
+    if not concatenate_flag:
+        front_clip.close()
+        end_clip.close()
 
 
-def select_video(piece_duration=300, front_clip_='front.mp4', end_clip_='end.mp4', output_folder="output",
-                 target_fps=25, concatenate_flag=False, height=872, width=1920, ffmpeg_codec="nvenc"):
+def select_video():
+    config = load_or_create_config()
+    piece_duration = int(config.get('PieceDuration', 300))
+    front_clip_ = config.get('FrontClip', 'front.mp4')
+    end_clip_ = config.get('EndClip', 'end.mp4')
+    output_folder = config.get('OutputFolder', 'output')
+    target_fps = int(config.get('TargetFPS', 30))
+    concatenate_flag = config.get('ConcatenateFlag', 'False') == 'True'
+    height = int(config.get('Height', 872))
+    width = int(config.get('Width', 1920))
+    ffmpeg_codec = config.get('FFmpegCodec', 'nvenc')
+    bitrate = config.get('Bitrate', '8000k')
+    concatenate_param = int(config.get('ConcatenateParam', 2))
+    fade_duration = int(config.get('FadeDuration', 2))
+
     def process_video():
-        # 获取输入参数信息
         piece_duration = int(piece_duration_entry.get())
         front_clip_ = front_clip_entry.get()
         end_clip_ = end_clip_entry.get()
@@ -47,14 +100,17 @@ def select_video(piece_duration=300, front_clip_='front.mp4', end_clip_='end.mp4
         height = int(height_entry.get())
         width = int(width_entry.get())
         ffmpeg_codec = ffmpeg_codec_var.get()
+        bitrate = bitrate_entry.get()
+        concatenate_param = int(concatenate_param_entry.get())
+        fade_duration = int(fade_duration_entry.get())
 
-        # 选择视频文件
         video_path = filedialog.askopenfilename(title="Select a video file", filetypes=[("MP4 files", "*.mp4")])
         if video_path:
             if video_path.endswith('.mp4'):
                 split_video(video_path, piece_duration=piece_duration, front_clip_=front_clip_, end_clip_=end_clip_,
                             output_folder=output_folder, target_fps=target_fps, concatenate_flag=concatenate_flag,
-                            height=height, width=width, ffmpeg_codec=ffmpeg_codec)
+                            height=height, width=width, ffmpeg_codec=ffmpeg_codec, bitrate=bitrate,
+                            concatenate_param=concatenate_param)
             else:
                 messagebox.showerror("Invalid File", "Please select an MP4 video file.")
 
@@ -74,7 +130,7 @@ def select_video(piece_duration=300, front_clip_='front.mp4', end_clip_='end.mp4
 
     root = tk.Tk()
     root.title("Video Processing Tool")
-    root.geometry("200x600")
+    root.geometry("200x700")
 
     # 参数输入框和标签
     piece_duration_label = tk.Label(root, text="Piece Duration (seconds):")
@@ -138,6 +194,24 @@ def select_video(piece_duration=300, front_clip_='front.mp4', end_clip_='end.mp4
     ffmpeg_codec_var.set(ffmpeg_codec)
     ffmpeg_codec_optionmenu = tk.OptionMenu(root, ffmpeg_codec_var, "nvenc", "h264_qsv", "h264_amf", "libx264")
     ffmpeg_codec_optionmenu.pack()
+
+    bitrate_label = tk.Label(root, text="Bitrate:")
+    bitrate_label.pack()
+    bitrate_entry = tk.Entry(root)
+    bitrate_entry.insert(tk.END, bitrate)
+    bitrate_entry.pack()
+
+    concatenate_param_label = tk.Label(root, text="Concatenate Parameter:")
+    concatenate_param_label.pack()
+    concatenate_param_entry = tk.Entry(root)
+    concatenate_param_entry.insert(tk.END, str(concatenate_param))
+    concatenate_param_entry.pack()
+
+    fade_duration_label = tk.Label(root, text="Fade Duration (seconds):")
+    fade_duration_label.pack()
+    fade_duration_entry = tk.Entry(root)
+    fade_duration_entry.insert(tk.END, str(fade_duration))
+    fade_duration_entry.pack()
 
     process_button = tk.Button(root, text="Process Video", command=process_video)
     process_button.pack()
